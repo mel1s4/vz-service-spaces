3070c6e00284b1df1d6d2e9efaa921cf37444992
Melisa Viroz <melisaviroz@gmail.com>
2025-01-28 08:51:36 -0800

    Prepping for upload

---
+++ zipit.sh
@@ -0,0 +1,9 @@
+rm -rf vz-service-spaces;
+rm -rf vz-service-spaces.zip;
+mkdir vz-service-spaces;
+find . -maxdepth 1 -type f -exec cp {} vz-service-spaces/ \;
+mkdir vz-service-spaces/service-space-single;
+mkdir vz-service-spaces/service-space-single/build;
+cp -r service-space-single/build vz-service-spaces/service-space-single/build;
+zip -r vz-service-spaces.zip vz-service-spaces;
+rm -rf vz-service-spaces;
